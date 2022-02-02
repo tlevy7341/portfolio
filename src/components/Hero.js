b907a45f1c2239bce6c55178b190c1e363feb203
Tyrail Leverett <tyrailleverett@gmail.com>
2022-02-01 20:21:22 -0600

    Capitalized the Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,41 +3,49 @@ import Particles from "react-tsparticles";
 import ParticlesConfig from "../utils/ParticlesConfig";
 import "../App.css";
 
-const Hero = ({myRefs}) => {
-    const [aboutRef,skillsRef,projectsRef] = myRefs;
-    const executeScroll = (ref) => ref.current.scrollIntoView({ behavior: 'smooth' })
+const Hero = ({ myRefs }) => {
+  const [aboutRef, skillsRef, projectsRef] = myRefs;
+  const executeScroll = (ref) =>
+    ref.current.scrollIntoView({ behavior: "smooth" });
 
-    return (
-        <>
-            <Particles
-                canvasClassName='particles_canvas'
-                options={ParticlesConfig}
-            />
-            <div id='hero' className="bg-darkbg flex flex-col justify-center items-center text-white w-screen h-screen">
-
-                <p className="font-bold font-roboto text-2xl sm:text-6xl z-10">
-                    Hi, I'm <span className="text-sky-600">Tyrail.</span>
-                </p>
-                <p className="font-bold font-roboto text-2xl sm:text-6xl z-10">
-                    I'm a full stack developer.
-                </p>
-                <div className="flex gap-4 items-center">
-                    <button onClick={()=> executeScroll(skillsRef)} className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10">
-                        Skills
-                    </button>
-                    <span>&#8226;</span>
-                    <button onClick={()=> executeScroll(projectsRef)} className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10">
-                        Projects
-                    </button>
-                    <span>&#8226;</span>
-                    <button onClick={()=> executeScroll(aboutRef)} className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10">
-                        About
-                    </button>
-                </div>
-            </div>
-
-        </>
-    );
+  return (
+    <>
+      <Particles canvasClassName="particles_canvas" options={ParticlesConfig} />
+      <div
+        id="hero"
+        className="bg-darkbg flex flex-col justify-center items-center text-white w-screen h-screen"
+      >
+        <p className="font-bold font-roboto text-2xl sm:text-6xl z-10">
+          Hi, I'm <span className="text-sky-600">Tyrail.</span>
+        </p>
+        <p className="font-bold font-roboto text-2xl sm:text-6xl z-10">
+          I'm a Full Stack Developer.
+        </p>
+        <div className="flex gap-4 items-center">
+          <button
+            onClick={() => executeScroll(skillsRef)}
+            className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10"
+          >
+            Skills
+          </button>
+          <span>&#8226;</span>
+          <button
+            onClick={() => executeScroll(projectsRef)}
+            className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10"
+          >
+            Projects
+          </button>
+          <span>&#8226;</span>
+          <button
+            onClick={() => executeScroll(aboutRef)}
+            className="underline font-semibold text-sm sm:text-xl hover:text-sky-600 z-10"
+          >
+            About
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
